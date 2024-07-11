27e1ab4b81d48ac409ffbba6ce3749bf9311a16f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-11 23:06:31 +0800

    public partial class JoinedStream : Stream 使用  public IAsyncEnumerator<Stream> StreamEnumerator { get; set; }

--- JCNET/流/JoinedStream.cs
+++ JCNET/流/JoinedStream.cs
@@ -3,8 +3,19 @@
 /// <summary>
 ///		能够将多个流在字节层面拼接在一起，形成一个字节流。
 /// </summary>
-public class JoinedStream : Stream
+public partial class JoinedStream : Stream
 {
+	public JoinedStream(IAsyncEnumerator<Stream> stream_enumerator)
+	{
+		StreamEnumerator = stream_enumerator;
+	}
+
+	public JoinedStream(IAsyncEnumerable<Stream> stream_souce)
+		: this(stream_souce.GetAsyncEnumerator())
+	{
+
+	}
+
 	private bool _disposed = false;
 	public override async ValueTask DisposeAsync()
 	{
@@ -22,6 +33,95 @@ public class JoinedStream : Stream
 			await _currentStream.DisposeAsync();
 		}
 	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="buffer"></param>
+	/// <param name="offset"></param>
+	/// <param name="count"></param>
+	/// <param name="cancellationToken"></param>
+	/// <returns></returns>
+	/// <exception cref="TaskCanceledException"></exception>
+	public override async Task<int> ReadAsync(byte[] buffer, int offset, int count,
+		CancellationToken cancellationToken)
+	{
+		Memory<byte> memory = new(buffer, offset, count);
+		return await ReadAsync(memory, cancellationToken);
+	}
+
+	public override async ValueTask<int> ReadAsync(Memory<byte> buffer,
+		CancellationToken cancellationToken = default)
+	{
+		while (true)
+		{
+			if (cancellationToken.IsCancellationRequested)
+			{
+				throw new TaskCanceledException();
+			}
+
+			// 当前流为空，需要加载一个新的流
+			if (_currentStream == null)
+			{
+				bool move_next_result = await StreamEnumerator.MoveNextAsync();
+				if (!move_next_result)
+				{
+					return 0;
+				}
+
+				_currentStream = StreamEnumerator.Current;
+			}
+
+			// 执行到这里说明 _currentStream 不为空。
+			int haveRead = await _currentStream.ReadAsync(buffer, cancellationToken);
+			if (haveRead == 0)
+			{
+				// 当前流已经结束了，先释放此流
+				await _currentStream.DisposeAsync();
+				_currentStream = null;
+				continue;
+			}
+
+			_position += haveRead;
+			return haveRead;
+		}
+	}
+
+	private Stream? _currentStream = null;
+	public IAsyncEnumerator<Stream> StreamEnumerator { get; set; }
+}
+
+public partial class JoinedStream
+{
+	#region 不支持的功能
+	public override long Length
+	{
+		get
+		{
+			throw new NotSupportedException();
+		}
+	}
+	public override void Flush()
+	{
+		throw new NotSupportedException();
+	}
+	public override int Read(byte[] buffer, int offset, int count)
+	{
+		throw new NotSupportedException();
+	}
+	public override long Seek(long offset, SeekOrigin origin)
+	{
+		throw new NotSupportedException();
+	}
+	public override void SetLength(long value)
+	{
+		throw new NotSupportedException();
+	}
+	public override void Write(byte[] buffer, int offset, int count)
+	{
+		throw new NotSupportedException();
+	}
+	#endregion
 
 	public override bool CanRead
 	{
@@ -60,171 +160,4 @@ public class JoinedStream : Stream
 			throw new NotSupportedException();
 		}
 	}
-
-	/// <summary>
-	///		尝试从队列和 IStreamSource 中获取新的流。如果两者都没有，返回 null
-	/// </summary>
-	/// <returns></returns>
-	private async Task<Stream?> TryGetNewStream()
-	{
-		try
-		{
-			// 如果队列空了，先触发 GetNextStreamAsync 然后再尝试退队
-			if (_streamQueue.Count == 0 && StreamSource != null)
-			{
-				await StreamSource.GetNextStreamAsync();
-			}
-
-			// 退队
-			lock (_streamQueue)
-			{
-				/* 如果 GetNextStreamAsync 后有流入队，则退队成功，否则退队失败，抛出异常*/
-				return _streamQueue.Dequeue();
-			}
-		}
-		catch
-		{
-			// 队列中已经没有流了，退队失败
-			return null;
-		}
-	}
-
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="buffer"></param>
-	/// <param name="offset"></param>
-	/// <param name="count"></param>
-	/// <param name="cancellationToken"></param>
-	/// <returns></returns>
-	/// <exception cref="TaskCanceledException"></exception>
-	public override async Task<int> ReadAsync(byte[] buffer, int offset,
-		int count, CancellationToken cancellationToken)
-	{
-		while (!cancellationToken.IsCancellationRequested)
-		{
-			// 当前流为空，需要加载一个新的流
-			if (_currentStream == null)
-			{
-				_currentStream = await TryGetNewStream();
-
-				// 尝试加载后仍然为空，返回 0，表示 JoinedStream 到达尽头
-				if (_currentStream == null)
-				{
-					return 0;
-				}
-			}
-
-			// 执行到这里说明 _currentStream 不为空。
-			int haveRead = await _currentStream.ReadAsync(buffer, cancellationToken);
-			if (haveRead == 0)
-			{
-				// 当前流已经结束了，先释放此流
-				_currentStream.Dispose();
-				_currentStream = null;
-				continue;
-			}
-
-			_position += haveRead;
-			return haveRead;
-		}
-
-		throw new TaskCanceledException();
-	}
-
-	public override async ValueTask<int> ReadAsync(Memory<byte> buffer,
-		CancellationToken cancellationToken = default)
-	{
-		// 当前流为空，需要加载一个新的流
-		if (_currentStream == null)
-		{
-			_currentStream = await TryGetNewStream();
-			// 尝试加载后仍然为空，返回 0，表示 JoinedStream 到达尽头
-			if (_currentStream == null)
-			{
-				return 0;
-			}
-		}
-
-		// 上面已经保证 _currentStream 不为空了（为空已经返回 0 了，执行不到这里），接下来只管读
-		int haveRead = await _currentStream.ReadAsync(buffer, cancellationToken);
-		if (haveRead > 0)
-		{
-			_position += haveRead;
-			return haveRead;
-		}
-		else
-		{
-			// 进入到这里只有可能是 haveRead == 0
-			// 此流已经结束了，先释放此流
-			_currentStream.Dispose();
-			_currentStream = null;
-			// 然后需要取出队列中的下一个流继续读取，这里使用递归
-			return await ReadAsync(buffer, cancellationToken);
-		}
-	}
-
-	#region 不支持的功能
-	public override long Length
-	{
-		get
-		{
-			throw new NotSupportedException();
-		}
-	}
-
-	public override void Flush()
-	{
-		throw new NotSupportedException();
-	}
-	public override int Read(byte[] buffer, int offset, int count)
-	{
-		throw new NotSupportedException();
-	}
-	public override long Seek(long offset, SeekOrigin origin)
-	{
-		throw new NotSupportedException();
-	}
-	public override void SetLength(long value)
-	{
-		throw new NotSupportedException();
-	}
-	public override void Write(byte[] buffer, int offset, int count)
-	{
-		throw new NotSupportedException();
-	}
-	#endregion
-
-	private readonly Queue<Stream> _streamQueue = new();
-	private Stream? _currentStream = null;
-
-	/// <summary>
-	/// 向队列的末尾添加流
-	/// </summary>
-	/// <param name="stream"></param>
-	public void AppendStream(Stream stream)
-	{
-		lock (_streamQueue)
-		{
-			_streamQueue.Enqueue(stream);
-		}
-	}
-
-	/// <summary>
-	///		用于向 JoinedStream 提供流。
-	/// </summary>
-	public interface IStreamSource
-	{
-		/// <summary>
-		///		JoinedStream 在当前流读到尽头的时候就会调用此函数。
-		/// </summary>
-		/// <remarks>
-		///		用户需要在此函数中需要调用 JoinedStream 的 AppendStream 函数，
-		///		否则本函数返回后 JoinedStream 就会结束。
-		/// </remarks>
-		/// <returns></returns>
-		Task GetNextStreamAsync();
-	}
-
-	public IStreamSource? StreamSource { get; set; }
 }
