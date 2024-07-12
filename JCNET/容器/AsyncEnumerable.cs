aecea66e866e369197df43568d071ea38e938488
hjc2000 <hjc.666@foxmail.com>
2024-07-12 17:22:39 +0800

    public class AsyncEnumerable<T> : IAsyncEnumerable<T>

---
+++ JCNET/容器/AsyncEnumerable.cs
@@ -0,0 +1,58 @@
+﻿namespace JCNET.容器;
+
+public class AsyncEnumerable<T> : IAsyncEnumerable<T>
+{
+	public AsyncEnumerable(IEnumerable<T> enumerable)
+	{
+		_enumerable = enumerable;
+	}
+
+	private IEnumerable<T> _enumerable;
+
+	public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+	{
+		return new AsyncEnumerator<T>(_enumerable.GetEnumerator());
+	}
+}
+
+/// <summary>
+///		接收同步迭代器，包装成异步迭代器。
+/// </summary>
+/// <typeparam name="T"></typeparam>
+internal class AsyncEnumerator<T> : IAsyncEnumerator<T>
+{
+	public AsyncEnumerator(IEnumerator<T> enumerator)
+	{
+		_enumerator = enumerator;
+	}
+
+	private IEnumerator<T> _enumerator;
+
+	public async ValueTask<bool> MoveNextAsync()
+	{
+		await Task.CompletedTask;
+		return _enumerator.MoveNext();
+	}
+
+	public T Current
+	{
+		get
+		{
+			return _enumerator.Current;
+		}
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		await Task.CompletedTask;
+		_enumerator.Dispose();
+	}
+}
+
+public static class IEnumerableExtension
+{
+	public static IAsyncEnumerable<T> AsIAsyncEnumerableEx<T>(this IEnumerable<T> enumerable)
+	{
+		return new AsyncEnumerable<T>(enumerable);
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,7 @@
+﻿using JCNET.容器;
+
+List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+await foreach (int item in list.AsIAsyncEnumerableEx())
+{
+	Console.WriteLine(item);
+}
