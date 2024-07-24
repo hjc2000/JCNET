5795509034d4c690d8c16a9b9f77077b0e68ba5f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-24 21:41:16 +0800

    public class StringBuilderLogWriter : TextWriter

--- JCNET/StringBuilderLogWriter.cs
+++ JCNET/StringBuilderLogWriter.cs
@@ -45,6 +45,25 @@ public class StringBuilderLogWriter : TextWriter
 	private readonly StringBuilder _sb = new();
 
 	/// <summary>
+	///		写 1 个字符。
+	/// </summary>
+	/// <remarks>
+	///		至少需要实现这个。这个重载是其他写函数的基础，本函数实现了，其他写函数就都能用了。
+	///		当然，其他写函数都是虚函数，也就是可以单独拎出来重载。
+	/// </remarks>
+	/// <param name="value"></param>
+	public override void Write(char value)
+	{
+		if (_sb.Length + 1 > MaxCapacity)
+		{
+			// 再追加 1 个字符，_sb 就溢出了，此时需要移除头部。
+			_sb.Remove(0, _sb.Length / 2);
+		}
+
+		_sb.Append(value);
+	}
+
+	/// <summary>
 	///		写一个字符串。
 	/// </summary>
 	/// <param name="value"></param>
@@ -63,19 +82,34 @@ public class StringBuilderLogWriter : TextWriter
 			return;
 		}
 
+		/* 执行到这里说明 _sb 至少在为空的状态下能够容纳整个 value，所以只要删除头部的子字符串，
+		 * 腾出空间就行了。
+		 */
 		if (_sb.Length + value.Length > MaxCapacity)
 		{
 			// 将 value 追加到 _sb 后会导致长度超过 MaxCapacity
-
-			// 计算需要将头部移除的长度
-			int remove_length = Math.Min(_sb.Length, value.Length);
-
-			// 一次至少移除一半，不要频繁进行小片段的移除，则会降低性能。
-			remove_length = Math.Max(remove_length, _sb.Length / 2);
-			_sb.Remove(0, remove_length);
+			RemoveHeadSafly(Math.Max(value.Length, _sb.Length / 2));
 		}
 
 		_sb.Append(value);
+	}
+
+	/// <summary>
+	///		安全地移除头部的指定长度的子字符串。
+	/// </summary>
+	/// <param name="length">
+	///		要移除的头部的子字符串的长度。
+	///		如果指定的值超过了现有字符串的长度，则会将现有字符串清空，不会导致异常。
+	/// </param>
+	public void RemoveHeadSafly(int length)
+	{
+		if (length > _sb.Length)
+		{
+			_sb.Clear();
+			return;
+		}
+
+		_sb.Remove(0, length);
 	}
 
 	/// <summary>
