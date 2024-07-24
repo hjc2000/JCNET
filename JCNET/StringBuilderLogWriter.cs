e46fcd74250b77e232568c96120562a2b17e72c6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-24 20:10:55 +0800

    public class StringBuilderLogWriter : TextWriter

--- JCNET/StringBuilderLogWriter.cs
+++ JCNET/StringBuilderLogWriter.cs
@@ -45,37 +45,37 @@ public class StringBuilderLogWriter : TextWriter
 	private readonly StringBuilder _sb = new();
 
 	/// <summary>
-	///		写入时会触发此事件。
-	/// </summary>
-	public event Action? WriteEvent;
-
-	/// <summary>
 	///		写一个字符串。
 	/// </summary>
 	/// <param name="value"></param>
 	public override void Write(string? value)
 	{
-		_sb.Append(value);
-		if (_sb.Length > MaxCapacity)
+		if (value is null)
 		{
-			_sb.Remove(0, _sb.Length - MaxCapacity);
+			return;
 		}
 
-		WriteEvent?.Invoke();
-	}
+		if (value.Length > MaxCapacity)
+		{
+			// 要放进来的字符串长度超过 MaxCapacity，则清空 _sb，然后将 value 的后段放进去。
+			_sb.Clear();
+			_sb.Append(value[(value.Length - MaxCapacity)..]);
+			return;
+		}
 
-	/// <summary>
-	///		冲洗时会触发此事件。
-	/// </summary>
-	public event Action? FlushEvent;
+		if (_sb.Length + value.Length > MaxCapacity)
+		{
+			// 将 value 追加到 _sb 后会导致长度超过 MaxCapacity
 
-	/// <summary>
-	///		冲洗。
-	/// </summary>
-	public override void Flush()
-	{
-		base.Flush();
-		FlushEvent?.Invoke();
+			// 计算需要将头部移除的长度
+			int remove_length = Math.Min(_sb.Length, value.Length);
+
+			// 一次至少移除一半，不要频繁进行小片段的移除，则会降低性能。
+			remove_length = Math.Max(remove_length, _sb.Length / 2);
+			_sb.Remove(0, remove_length);
+		}
+
+		_sb.Append(value);
 	}
 
 	/// <summary>
