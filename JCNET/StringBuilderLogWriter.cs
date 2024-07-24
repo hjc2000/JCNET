ee91652c3d5bc3dc4be1853439c9b6fb1c6ecad3
hjc2000 <hjc.666@foxmail.com>
2024-07-24 15:24:03 +0800

    public class StringBuilderLogWriter : TextWriter

---
+++ JCNET/StringBuilderLogWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace JCNET;
+
+/// <summary>
+///		基于 StringBuilder 的 TextWriter。
+/// </summary>
+public class StringBuilderLogWriter : TextWriter
+{
+	/// <summary>
+	///		字符编码。本类为：Encoding.Unicode
+	/// </summary>
+	public override Encoding Encoding
+	{
+		get
+		{
+			return Encoding.Unicode;
+		}
+	}
+
+	private readonly StringBuilder _sb = new();
+
+	/// <summary>
+	///		写一个字符串。
+	/// </summary>
+	/// <param name="value"></param>
+	public override void Write(string? value)
+	{
+		_sb.Append(value);
+	}
+
+	/// <summary>
+	///		返回 StringBuilder 构建的字符串。
+	/// </summary>
+	/// <returns></returns>
+	public override string ToString()
+	{
+		return _sb.ToString();
+	}
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,10 @@
-﻿using JCNET.数学;
+﻿using JCNET;
 
-ScientificNotation sc = new(0, 3);
-Console.WriteLine(sc.ToString());
+StringBuilderLogWriter writer = new();
+TextWriter origin_out = Console.Out;
+Console.SetOut(writer);
+Console.WriteLine("666666666666");
+Console.SetOut(origin_out);
+
+string writer_string = writer.ToString();
+Console.WriteLine(writer_string);
