429db711dd5d893b4a7f339a1b9bdc9a9d9e56ca
hjc2000 <hjc.666@foxmail.com>
2024-07-24 11:50:44 +0800

    <Version>9.1.1</Version>

--- JCNET/反射/EnumReflex.cs
+++ JCNET/反射/EnumReflex.cs
@@ -10,14 +10,14 @@ public static class EnumReflex
 	/// </summary>
 	/// <typeparam name="TEnum"></typeparam>
 	/// <returns></returns>
-	public static Dictionary<int, string> ToDictionary<TEnum>() where TEnum : Enum
+	public static Dictionary<TEnum, string> ToDictionary<TEnum>() where TEnum : Enum
 	{
-		Dictionary<int, string> values = Enum.GetValues(typeof(TEnum))
+		Dictionary<TEnum, string> values = Enum.GetValues(typeof(TEnum))
 			.Cast<TEnum>()
 			.ToDictionary(
 				(TEnum e) =>
 				{
-					return Convert.ToInt32(e);
+					return e;
 				},
 				(TEnum e) =>
 				{
