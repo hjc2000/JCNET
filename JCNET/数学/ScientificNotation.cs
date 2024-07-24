172b5ded7ce80fc0908a376f81fb7ce5739cef12
hjc2000 <hjc.666@foxmail.com>
2024-07-24 11:42:08 +0800

    public class ScientificNotation

--- JCNET/数学/ScientificNotation.cs
+++ JCNET/数学/ScientificNotation.cs
@@ -2,23 +2,38 @@
 
 /// <summary>
 ///		表示 Base * 10 ^ Exponent 这种科学计数法的数
+///		
+///		<br/>*	在值等于 0 时，认为有效数字始终是 1 位。但是，在测量中，量纲为 m 的时候，
+///				(0 * 10 ^ 0) m 和 (0 * 10 ^ -3) m 是有区别的。前者表示在 m 的精度上
+///				测量结果是 0，后者表示在 mm 精度上测量，结果仍然是 0.
+///				这个时候本类的有效数字位数就变成用来指示精度量级。例如 (0 * 10 ^ -3) 对应
+///				SignificantDigits = 3.
 /// </summary>
 public class ScientificNotation
 {
+	/// <summary>
+	///		无参构造函数。
+	///		构造出来表示 0 * 10 ^ 0
+	/// </summary>
 	public ScientificNotation()
 	{
+		SignificantDigits = 0;
 		Base = 0;
 		Exponent = 0;
-		SignificantDigits = 1;
 	}
 
+	/// <summary>
+	///		构造函数
+	/// </summary>
+	/// <param name="num">值</param>
+	/// <param name="significant_digits">有效数字位数</param>
 	public ScientificNotation(double num, int significant_digits)
 	{
+		SignificantDigits = significant_digits;
 		if (num == 0)
 		{
 			Base = 0;
 			Exponent = 0;
-			SignificantDigits = 1;
 			return;
 		}
 
@@ -32,11 +47,16 @@ public class ScientificNotation
 			Exponent--;
 			Base *= 10;
 		}
-
-		SignificantDigits = significant_digits;
 	}
 
+	/// <summary>
+	///		基数
+	/// </summary>
 	public double Base { get; }
+
+	/// <summary>
+	///		10 的指数
+	/// </summary>
 	public int Exponent { get; }
 
 	/// <summary>
@@ -44,13 +64,26 @@ public class ScientificNotation
 	/// </summary>
 	public int SignificantDigits { get; }
 
+	/// <summary>
+	///		转化为 double
+	/// </summary>
+	/// <returns></returns>
 	public double ToDouble()
 	{
 		return Base * System.Math.Pow(10, Exponent);
 	}
 
+	/// <summary>
+	///		转化为类似 0.1 * 10 ^ 1 这样的字符串。
+	/// </summary>
+	/// <returns></returns>
 	public override string ToString()
 	{
+		if (Base == 0)
+		{
+			return $"{Base} * 10 ^ {-SignificantDigits}";
+		}
+
 		string base_str = Base.ToString();
 
 		// 规范化的科学计数法中，有效长度，即去掉了小数点后的长度，就会等于有效数字位数。
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,4 @@
 ﻿using JCNET.数学;
 
-ScientificNotation sc = new(0.002, 3);
+ScientificNotation sc = new(0, 3);
 Console.WriteLine(sc.ToString());
