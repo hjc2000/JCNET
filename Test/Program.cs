f8e801462766097db3f3a96ca9d7fba3eb5f473e
hjc2000 <hjc.666@foxmail.com>
2024-07-25 08:59:55 +0800

    Uri uri1 = new("https://fanyi.baidu.com/p/"); Uri uri2 = new("https://fanyi.baidu.com/p"); Console.WriteLine(uri1.IsBaseOf(uri2)); Console.WriteLine(uri1 == uri2);

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,4 @@
-﻿using JCNET.字符串处理;
-
-RoutePath path1 = new(new Uri("https://fanyi.baidu.com/"));
-RoutePath path2 = new(new Uri("https://fanyi.baidu.com/p"));
-
-Console.WriteLine(path1);
-Console.WriteLine(path2);
-Console.WriteLine(path2 == path1);
-Console.WriteLine(path2 < path1);
+﻿Uri uri1 = new("https://fanyi.baidu.com/p/");
+Uri uri2 = new("https://fanyi.baidu.com/p");
+Console.WriteLine(uri1.IsBaseOf(uri2));
+Console.WriteLine(uri1 == uri2);
