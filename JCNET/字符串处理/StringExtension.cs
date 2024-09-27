d8ed727658b6c28f14494283401c78b3337bf5ed
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-27 17:18:06 +0800

    ReplaceTwoWord

--- JCNET/字符串处理/StringExtension.cs
+++ JCNET/字符串处理/StringExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace JCNET.字符串处理;
+﻿using System.Text;
+
+namespace JCNET.字符串处理;
 
 /// <summary>
 ///		字符串扩展
@@ -326,111 +328,145 @@ public static class StringExtension
 		return self.AsMemory().CutMiddleWholeMatch(middle);
 	}
 
-	///// <summary>
-	/////		全字匹配替换
-	///// </summary>
-	///// <param name="self"></param>
-	///// <param name="match"></param>
-	///// <param name="replacement"></param>
-	///// <returns></returns>
-	//public static string ReplaceWholeMatch(this string self, string match, string replacement)
-	//{
-	//	if (match == replacement)
-	//	{
-	//		return self;
-	//	}
+	/// <summary>
+	///		全字匹配替换
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match"></param>
+	/// <param name="replacement"></param>
+	/// <returns></returns>
+	public static ReadOnlyMemory<char> ReplaceWholeMatch(this ReadOnlyMemory<char> self,
+		ReadOnlySpan<char> match, ReadOnlySpan<char> replacement)
+	{
+		if (match == replacement)
+		{
+			return self;
+		}
 
-	//	ReadOnlyMemory<char> remain = self.AsMemory();
-	//	StringBuilder sb = new();
-	//	while (true)
-	//	{
-	//		CuttingMiddleResult result = remain.CutMiddleWholeMatch(match);
-	//		if (!result.Success)
-	//		{
-	//			sb.Append(remain);
-	//			return sb.ToString();
-	//		}
+		StringBuilder sb = new();
+		ReadOnlyMemory<char> remain = self;
+		while (true)
+		{
+			CuttingMiddleResult result = remain.CutMiddleWholeMatch(match);
+			if (!result.Success)
+			{
+				sb.Append(remain);
+				return sb.ToString().AsMemory();
+			}
 
-	//		sb.Append($"{result.Left}{replacement}");
-	//		remain = result.Right;
-	//	}
-	//}
+			sb.Append(result.Left);
+			sb.Append(replacement);
+			remain = result.Right;
+		}
+	}
 
-	///// <summary>
-	/////		查找字符串中出现 left_word 和 right_word 相邻的，两者之间仅由空白字符分隔的部分，
-	/////		将其替换为 replacement。
-	/////		<br/>* 例如：hello      world 的 2 个单词之间出现很多空格，换行，缩进等，
-	/////			   本函数能够识别出来，将这两个单词及其中间部分给替换成 replacement。
-	///// </summary>
-	///// <param name="self"></param>
-	///// <param name="left_word"></param>
-	///// <param name="right_word"></param>
-	///// <param name="replacement"></param>
-	///// <returns></returns>
-	//public static string ReplaceTwoWord(this string self,
-	//	string left_word, string right_word, string replacement)
-	//{
-	//	int offset = 0;
-	//	StringBuilder sb = new();
-	//	while (true)
-	//	{
-	//		if (offset >= self.Length)
-	//		{
-	//			return sb.ToString();
-	//		}
+	/// <summary>
+	///		全字匹配替换
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match"></param>
+	/// <param name="replacement"></param>
+	/// <returns></returns>
+	public static ReadOnlyMemory<char> ReplaceWholeMatch(this string self,
+		ReadOnlySpan<char> match, ReadOnlySpan<char> replacement)
+	{
+		return self.AsMemory().ReplaceWholeMatch(match, replacement);
+	}
 
-	//		int left_word_index = self.IndexOf(left_word, offset);
-	//		if (left_word_index == -1)
-	//		{
-	//			sb.Append(self, offset, self.Length - offset);
-	//			return sb.ToString();
-	//		}
+	/// <summary>
+	///		查找字符串中出现 left_word 和 right_word 相邻的，两者之间仅由空白字符分隔的部分，
+	///		将其替换为 replacement。
+	///		<br/>* 例如：hello      world 的 2 个单词之间出现很多空格，换行，缩进等，
+	///			   本函数能够识别出来，将这两个单词及其中间部分给替换成 replacement。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="left_word"></param>
+	/// <param name="right_word"></param>
+	/// <param name="replacement"></param>
+	/// <returns></returns>
+	public static ReadOnlyMemory<char> ReplaceTwoWord(this ReadOnlyMemory<char> self,
+		ReadOnlySpan<char> left_word, ReadOnlySpan<char> right_word,
+		ReadOnlySpan<char> replacement)
+	{
+		int offset = 0;
+		StringBuilder sb = new();
+		while (true)
+		{
+			if (offset >= self.Length)
+			{
+				return sb.ToString().AsMemory();
+			}
 
-	//		if (left_word_index > 0)
-	//		{
-	//			// 找到了，并且不是开头，需要检查左边一个字符
-	//			if (!self[left_word_index - 1].IsWordSeperation())
-	//			{
-	//				// 左边一个字符不是分隔符
-	//				sb.Append(self, offset, left_word_index + left_word.Length - offset);
-	//				offset = left_word_index + left_word.Length;
-	//				continue;
-	//			}
-	//		}
+			int left_word_index = self.IndexOf(left_word, offset);
+			if (left_word_index == -1)
+			{
+				sb.Append(self[offset..]);
+				return sb.ToString().AsMemory();
+			}
 
-	//		int right_word_index = self.IndexOf(right_word, left_word_index + left_word.Length);
-	//		if (right_word_index == -1)
-	//		{
-	//			sb.Append(self, offset, self.Length - offset);
-	//			return sb.ToString();
-	//		}
+			if (left_word_index > 0)
+			{
+				// 找到了，并且不是开头，需要检查左边一个字符
+				if (!self.Span[left_word_index - 1].IsWordSeperation())
+				{
+					// 左边一个字符不是分隔符
+					sb.Append(self[offset..(left_word_index + left_word.Length)]);
+					offset = left_word_index + left_word.Length;
+					continue;
+				}
+			}
 
-	//		if (right_word_index + right_word.Length < self.Length)
-	//		{
-	//			// 找到了，并且不是最后一个字符，需要检查右边一个字符
-	//			if (!self[right_word_index + right_word.Length].IsWordSeperation())
-	//			{
-	//				// 右边一个字符不是分隔符
-	//				sb.Append(self, offset, right_word_index + right_word.Length - offset);
-	//				offset = right_word_index + right_word.Length;
-	//				continue;
-	//			}
-	//		}
+			int right_word_index = self.IndexOf(right_word, left_word_index + left_word.Length);
+			if (right_word_index == -1)
+			{
+				sb.Append(self[offset..]);
+				return sb.ToString().AsMemory();
+			}
 
-	//		if (self.ContainsNotWhiteSpaceChar(left_word_index + left_word.Length, right_word_index))
-	//		{
-	//			// 左单词和右单词之间存在非空白字符
-	//			sb.Append(self, offset, left_word_index + left_word.Length - offset);
-	//			offset = left_word_index + left_word.Length;
-	//			continue;
-	//		}
+			if (right_word_index + right_word.Length < self.Length)
+			{
+				// 找到了，并且不是最后一个字符，需要检查右边一个字符
+				if (!self.Span[right_word_index + right_word.Length].IsWordSeperation())
+				{
+					// 右边一个字符不是分隔符
+					sb.Append(self[offset..(right_word_index + right_word.Length)]);
+					offset = right_word_index + right_word.Length;
+					continue;
+				}
+			}
 
-	//		// 经过了一道道关卡，到这里可以进行替换了
-	//		sb.Append(self, offset, left_word_index - offset);
-	//		sb.Append(replacement);
-	//		offset = right_word_index + right_word.Length;
-	//	} //while (true)
-	//}
+			if (self.ContainsNotWhiteSpaceChar(left_word_index + left_word.Length, right_word_index))
+			{
+				// 左单词和右单词之间存在非空白字符
+				sb.Append(self[offset..(left_word_index + left_word.Length)]);
+				offset = left_word_index + left_word.Length;
+				continue;
+			}
+
+			// 经过了一道道关卡，到这里可以进行替换了
+			sb.Append(self[offset..left_word_index]);
+			sb.Append(replacement);
+			offset = right_word_index + right_word.Length;
+		} //while (true)
+	}
+
+	/// <summary>
+	///		查找字符串中出现 left_word 和 right_word 相邻的，两者之间仅由空白字符分隔的部分，
+	///		将其替换为 replacement。
+	///		<br/>* 例如：hello      world 的 2 个单词之间出现很多空格，换行，缩进等，
+	///			   本函数能够识别出来，将这两个单词及其中间部分给替换成 replacement。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="left_word"></param>
+	/// <param name="right_word"></param>
+	/// <param name="replacement"></param>
+	/// <returns></returns>
+	public static ReadOnlyMemory<char> ReplaceTwoWord(this string self,
+		ReadOnlySpan<char> left_word, ReadOnlySpan<char> right_word,
+		ReadOnlySpan<char> replacement)
+	{
+		return self.AsMemory().ReplaceTwoWord(left_word, right_word, replacement);
+	}
 }
 
 /// <summary>
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,74 +1,64 @@
-﻿//using JCNET.字符串处理;
+﻿using JCNET.字符串处理;
 
-//string str = @"
-//	--- 定位、定速运行
-//	if true then
-//		--- 设置转速并按此转速运行。
-//		--- 可以为正负数或 0。设置为正数则正转，负数则反转，0 则停止。
-//		--- 单位：rpm。
-//		--- @param value number 浮点数。
-//		function Servo.SetSpeedAndRun(value)
-//			-- 要设置的速度如果和当前转速反向了，先断开正转、反转信号。
-//			if (Servo.Feedback.Speed() * value < 0) then
-//				Servo.EI.SetForwardSignal(false)
-//				Servo.EI.SetReverseSignal(false)
-//			end
+string str = @"
+	--- 定位、定速运行
+	if true then
+		--- 设置转速并按此转速运行。
+		--- 可以为正负数或 0。设置为正数则正转，负数则反转，0 则停止。
+		--- 单位：rpm。
+		--- @param value number 浮点数。
+		function Servo.SetSpeedAndRun(value)
+			-- 要设置的速度如果和当前转速反向了，先断开正转、反转信号。
+			if (Servo.Feedback.Speed() * value < 0) then
+				Servo.EI.SetForwardSignal(false)
+				Servo.EI.SetReverseSignal(false)
+			end
 
-//			Servo.Param.SetSpeedLimit(value)
-//			AXIS_SPEED(value)
+			Servo.Param.SetSpeedLimit(value)
+			AXIS_SPEED(value)
 
-//			if (value > 0) then
-//				-- 正转
-//				Servo.EI.SetForwardSignal(true)
-//				Servo.EI.SetReverseSignal(false)
-//			elseif (value < 0) then
-//				-- 反转
-//				Servo.EI.SetForwardSignal(false)
-//				Servo.EI.SetReverseSignal(true)
-//			end
-//		end
+			if (value > 0) then
+				-- 正转
+				Servo.EI.SetForwardSignal(true)
+				Servo.EI.SetReverseSignal(false)
+			elseif (value < 0) then
+				-- 反转
+				Servo.EI.SetForwardSignal(false)
+				Servo.EI.SetReverseSignal(true)
+			end
+		end
 
-//		--- 设置绝对定位立即数并运行。
-//		--- @param value integer
-//		function Servo.SetAbsolutePositionAndRun(value)
-//			print(""SetAbsolutePositionAndRun 定位到"", value)
-//			AXIS_MOVEABS(value)
-//			Servo.EI.TriggerRisingEdge(14)
-//		end
+		--- 设置绝对定位立即数并运行。
+		--- @param value integer
+		function Servo.SetAbsolutePositionAndRun(value)
+			print(""SetAbsolutePositionAndRun 定位到"", value)
+			AXIS_MOVEABS(value)
+			Servo.EI.TriggerRisingEdge(14)
+		end
 
-//		--- 通过 EI 进行位置预置，设置零点
-//		function Servo.PresetPosition()
-//			Servo.EI.TriggerRisingEdge(15)
-//		end
+		--- 通过 EI 进行位置预置，设置零点
+		function Servo.PresetPosition()
+			Servo.EI.TriggerRisingEdge(15)
+		end
 
-//		--- 取消定位
-//		function Servo.CancelPositioning()
-//			AXIS_CANCEL()
-//		end
+		--- 取消定位
+		function Servo.CancelPositioning()
+			AXIS_CANCEL()
+		end
 
-//		--- 停止转动。
-//		--- 切换到速度模式，并将速度锁定为 0. 无论原来是什么模式（定位模式、速度模式、转矩模式）都可以使用。
-//		function Servo.Stop()
-//			Servo.ChangeToSpeedMode()
-//			Servo.SetSpeedAndRun(0)
-//		end
-//	end
-//";
+		--- 停止转动。
+		--- 切换到速度模式，并将速度锁定为 0. 无论原来是什么模式（定位模式、速度模式、转矩模式）都可以使用。
+		function Servo.Stop()
+			Servo.ChangeToSpeedMode()
+			Servo.SetSpeedAndRun(0)
+		end
+	end
+";
 
-//int i = 0;
-//str = str.ReplaceTwoWord("function", "Servo.SetSpeedAndRun", $"G[{i++}] = function");
-//str = str.ReplaceTwoWord("function", "Servo.SetAbsolutePositionAndRun", $"G[{i++}] = function");
-//str = str.ReplaceTwoWord("function", "Servo.PresetPosition", $"G[{i++}] = function");
-//str = str.ReplaceTwoWord("function", "Servo.CancelPositioning", $"G[{i++}] = function");
-//str = str.ReplaceTwoWord("function", "Servo.Stop", $"G[{i++}] = function");
-//Console.WriteLine(str);
-
-using JCNET.字符串处理;
-
-string str = "666中间777-666-中间-777";
-CuttingMiddleResult result = str.CutMiddle("中间");
-if (result.Success)
-{
-	Console.WriteLine(result.Left);
-	Console.WriteLine(result.Right);
-}
+int i = 0;
+ReadOnlyMemory<char> memory = str.ReplaceTwoWord("function", "Servo.SetSpeedAndRun", $"G[{i++}] = function");
+memory = memory.ReplaceTwoWord("function", "Servo.SetAbsolutePositionAndRun", $"G[{i++}] = function");
+memory = memory.ReplaceTwoWord("function", "Servo.PresetPosition", $"G[{i++}] = function");
+memory = memory.ReplaceTwoWord("function", "Servo.CancelPositioning", $"G[{i++}] = function");
+memory = memory.ReplaceTwoWord("function", "Servo.Stop", $"G[{i++}] = function");
+Console.WriteLine(memory);
