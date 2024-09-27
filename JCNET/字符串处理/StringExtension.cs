840070599e3db64512fbf339fb047e84b85955b4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-27 19:11:04 +0800

    <Version>14.0.0</Version>

--- JCNET/字符串处理/StringExtension.cs
+++ JCNET/字符串处理/StringExtension.cs
@@ -109,36 +109,25 @@ public static class StringExtension
 	/// <param name="self"></param>
 	/// <param name="match">从这里之前的部分要被截掉。</param>
 	/// <param name="trim_match">截掉的部分是否包括 match 本身。true 表示包括，false 表示不包括。</param>
-	/// <returns></returns>
-	public static TrimResult TrimHeadBeforeFirstMatch(this ReadOnlyMemory<char> self,
+	/// <returns>返回去掉头部之后的结果。如果 match 没有匹配，则没有头部，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimHeadBeforeFirstMatch(this ReadOnlyMemory<char> self,
 		ReadOnlySpan<char> match, bool trim_match)
 	{
-		int index = self.Span.IndexOf(match);
+		int index = self.IndexOf(match);
 		if (index == -1)
 		{
-			return new TrimResult()
-			{
-				Success = false,
-			};
+			return self;
 		}
 
 		if (trim_match)
 		{
 			// 截掉的部分包括 match 本身
-			return new TrimResult()
-			{
-				Success = true,
-				Remain = self[(index + match.Length)..],
-			};
+			return self[(index + match.Length)..];
 		}
 		else
 		{
 			// 截掉的部分不包括 match 本身
-			return new TrimResult()
-			{
-				Success = true,
-				Remain = self[index..],
-			};
+			return self[index..];
 		}
 	}
 
@@ -149,8 +138,8 @@ public static class StringExtension
 	/// <param name="self"></param>
 	/// <param name="match"></param>
 	/// <param name="trim_match"></param>
-	/// <returns></returns>
-	public static TrimResult TrimHeadBeforeFirstMatch(this string self,
+	/// <returns>返回去掉头部之后的结果。如果 match 没有匹配，则没有头部，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimHeadBeforeFirstMatch(this string self,
 		ReadOnlySpan<char> match, bool trim_match)
 	{
 		return self.AsMemory().TrimHeadBeforeFirstMatch(match, trim_match);
@@ -163,34 +152,23 @@ public static class StringExtension
 	/// <param name="self"></param>
 	/// <param name="match">从这里往后的尾巴将会被截掉。</param>
 	/// <param name="trim_match">截掉的部分是否包括 match 本身。true 表示包括，false 表示不包括。</param>
-	/// <returns></returns>
-	public static TrimResult TrimTailAfterFirstMatch(this ReadOnlyMemory<char> self,
+	/// <returns>返回去掉尾巴后的结果。如果 match 没有匹配，则没有尾巴，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimTailAfterFirstMatch(this ReadOnlyMemory<char> self,
 		ReadOnlySpan<char> match, bool trim_match)
 	{
 		int index = self.Span.IndexOf(match);
 		if (index == -1)
 		{
-			return new TrimResult()
-			{
-				Success = false,
-			};
+			return self;
 		}
 
 		if (trim_match)
 		{
-			return new TrimResult()
-			{
-				Success = true,
-				Remain = self[..index],
-			};
+			return self[..index];
 		}
 		else
 		{
-			return new TrimResult()
-			{
-				Success = true,
-				Remain = self[..(index + match.Length)],
-			};
+			return self[..(index + match.Length)];
 		}
 	}
 
@@ -201,48 +179,48 @@ public static class StringExtension
 	/// <param name="self"></param>
 	/// <param name="match"></param>
 	/// <param name="trim_match"></param>
-	/// <returns></returns>
-	public static TrimResult TrimTailAfterFirstMatch(this string self,
+	/// <returns>返回去掉尾巴后的结果。如果 match 没有匹配，则没有尾巴，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimTailAfterFirstMatch(this string self,
 		ReadOnlySpan<char> match, bool trim_match)
 	{
 		return self.AsMemory().TrimTailAfterFirstMatch(match, trim_match);
 	}
 
 	/// <summary>
-	///		剪切字符串。
-	///		<br/>* 将字符串掐头去尾，取出位于 start_string 和 end_string 之间的子字符串，
-	///			   不包括 start_string 和 end_string 的内容。
+	///		将字符串掐头去尾，取出位于 start_string 和 end_string 之间的子字符串，
+	///		不包括 start_string 和 end_string 的内容。
 	/// </summary>
 	/// <param name="self"></param>
 	/// <param name="start_string"></param>
 	/// <param name="end_string"></param>
-	/// <returns></returns>
-	public static TrimResult Cut(this ReadOnlyMemory<char> self,
+	/// <returns>如果 start_string 和 end_string 都不存在，则返回空字符串。</returns>
+	public static ReadOnlyMemory<char> GetBetween(this ReadOnlyMemory<char> self,
 		ReadOnlySpan<char> start_string, ReadOnlySpan<char> end_string)
 	{
-		TrimResult result = self.TrimHeadBeforeFirstMatch(start_string, true);
-		if (!result.Success)
+		int origin_length = self.Length;
+		self = self.TrimHeadBeforeFirstMatch(start_string, true);
+		self = self.TrimTailAfterFirstMatch(end_string, true);
+		if (self.Length == origin_length)
 		{
-			return result;
+			// 经过裁剪后长度没有任何变化，说明 start_string 和 end_string 都不存在
+			return string.Empty.AsMemory();
 		}
 
-		result = result.Remain.TrimTailAfterFirstMatch(end_string, true);
-		return result;
+		return self;
 	}
 
 	/// <summary>
-	///		剪切字符串。
-	///		<br/>* 将字符串掐头去尾，取出位于 start_string 和 end_string 之间的子字符串，
-	///			   不包括 start_string 和 end_string 的内容。
+	///		将字符串掐头去尾，取出位于 start_string 和 end_string 之间的子字符串，
+	///		不包括 start_string 和 end_string 的内容。
 	/// </summary>
 	/// <param name="self"></param>
 	/// <param name="start_string"></param>
 	/// <param name="end_string"></param>
-	/// <returns></returns>
-	public static TrimResult Cut(this string self,
+	/// <returns>如果 start_string 和 end_string 都不存在，则返回空字符串。</returns>
+	public static ReadOnlyMemory<char> GetBetween(this string self,
 		ReadOnlySpan<char> start_string, ReadOnlySpan<char> end_string)
 	{
-		return self.AsMemory().Cut(start_string, end_string);
+		return self.AsMemory().GetBetween(start_string, end_string);
 	}
 
 	/// <summary>
@@ -495,22 +473,6 @@ public static class StringExtension
 	{
 		return self.AsMemory().ReplaceTwoWord(left_word, right_word, replacement);
 	}
-}
-
-/// <summary>
-///		去除内容后的结果
-/// </summary>
-public class TrimResult
-{
-	/// <summary>
-	///		去除内容成功
-	/// </summary>
-	public bool Success { get; set; } = false;
-
-	/// <summary>
-	///		去除内容成功后，剩余的部分放在这里
-	/// </summary>
-	public ReadOnlyMemory<char> Remain { get; set; } = new Memory<char>();
 }
 
 /// <summary>
