2498c22814d0a909d0f64e6de312611c8609d6ac
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-30 00:32:48 +0800

    public static List<string> GetCustomGlobalVariableNames(this NLua.Lua self)

--- JCNET/Lua/LuaExtension.cs
+++ JCNET/Lua/LuaExtension.cs
@@ -37,4 +37,22 @@ public static class LuaExtension
 
 		return key_lists;
 	}
+
+	/// <summary>
+	///		获取自定义的全局变量的变量名列表。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <returns></returns>
+	public static List<string> GetCustomGlobalVariableNames(this NLua.Lua self)
+	{
+		NLua.Lua lua = new();
+		List<string> origin_global_variable_names = lua.GetGlobalVariableNames();
+		List<string> current_global_variable_names = self.GetGlobalVariableNames();
+		foreach (string name in origin_global_variable_names)
+		{
+			current_global_variable_names.Remove(name);
+		}
+
+		return current_global_variable_names;
+	}
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -2,7 +2,10 @@
 using NLua;
 
 Lua lua = new();
-List<string> list = lua.GetGlobalVariableNames();
+lua.DoString(@"
+	Servo = {}
+");
+List<string> list = lua.GetCustomGlobalVariableNames();
 foreach (string s in list)
 {
 	Console.WriteLine(s);
