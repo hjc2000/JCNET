979d0148f5ad4ab25e9338e3385f28e3e7dc39d1
hjc2000 <hjc.666@foxmail.com>
2024-09-29 15:22:47 +0800

    将扩展方法分离到不同文件

---
+++ JCNET/字符串处理/CutExtension.cs
@@ -0,0 +1,137 @@
+﻿namespace JCNET.字符串处理;
+
+/// <summary>
+///		切割字符串的扩展
+/// </summary>
+public static class CutExtension
+{
+	/// <summary>
+	///		将字符串切除中间部分，留下左边和右边的部分。
+	///		<br/>* middle 如果为空字符串，则会导致切除中间失败，这根本没法切。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="middle"></param>
+	/// <returns></returns>
+	public static CuttingMiddleResult CutMiddle(this ReadOnlyMemory<char> self, ReadOnlySpan<char> middle)
+	{
+		if (self.Length == 0 || middle.Length == 0)
+		{
+			return new CuttingMiddleResult()
+			{
+				Success = false,
+			};
+		}
+
+		int index = self.IndexOf(middle);
+		if (index == -1)
+		{
+			return new CuttingMiddleResult()
+			{
+				Success = false,
+			};
+		}
+
+		return new CuttingMiddleResult()
+		{
+			Success = true,
+			Left = self[..index],
+			Right = self[(index + middle.Length)..]
+		};
+	}
+
+	/// <summary>
+	///		将字符串切除中间部分，留下左边和右边的部分。
+	///		<br/>* middle 如果为空字符串，则会导致切除中间失败，这根本没法切。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="middle"></param>
+	/// <returns></returns>
+	public static CuttingMiddleResult CutMiddle(this string self, ReadOnlySpan<char> middle)
+	{
+		return self.AsMemory().CutMiddle(middle);
+	}
+
+	/// <summary>
+	///		全字匹配地切除中间部分
+	///		<br/>* middle 如果为空字符串，则会导致切除中间失败，这根本没法切。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="middle"></param>
+	/// <returns></returns>
+	public static CuttingMiddleResult CutMiddleWholeMatch(this ReadOnlyMemory<char> self,
+		ReadOnlySpan<char> middle)
+	{
+		if (self.Length == 0 || middle.Length == 0)
+		{
+			return new CuttingMiddleResult()
+			{
+				Success = false,
+			};
+		}
+
+		int finding_offset = 0;
+		while (true)
+		{
+			if (finding_offset >= self.Length)
+			{
+				return new CuttingMiddleResult()
+				{
+					Success = false,
+				};
+			}
+
+			int index = self.IndexOf(middle, finding_offset);
+			if (index == -1)
+			{
+				return new CuttingMiddleResult()
+				{
+					Success = false,
+				};
+			}
+
+			// 找到了，需要进一步验证是否全字匹配
+			if (index > 0)
+			{
+				// middle 不是在开头位置，需要检查前一个字符
+				if (!self.Span[index - 1].IsWordSeperation())
+				{
+					// 前一个字符不是分隔符，不满足全字匹配
+					finding_offset = index + middle.Length;
+					continue;
+				}
+			}
+
+			if (index + middle.Length < self.Length)
+			{
+				// middle 不是在结尾位置，需要检查后一个字符
+				if (!self.Span[index + middle.Length].IsWordSeperation())
+				{
+					// 后一个字符不是分隔符，不满足全字匹配
+					finding_offset = index + middle.Length;
+					continue;
+				}
+			}
+
+			// 经过验证，是全字匹配的
+			return new CuttingMiddleResult()
+			{
+				Success = true,
+				Left = self[..index],
+				Right = self[(index + middle.Length)..],
+			};
+		}
+	}
+
+	/// <summary>
+	///		全字匹配地切除中间部分
+	///		<br/>* middle 如果为空字符串，则会导致切除中间失败，这根本没法切。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="middle"></param>
+	/// <returns></returns>
+	public static CuttingMiddleResult CutMiddleWholeMatch(this string self,
+		ReadOnlySpan<char> middle)
+	{
+		return self.AsMemory().CutMiddleWholeMatch(middle);
+	}
+}
---
+++ JCNET/字符串处理/TrimExtension.cs
@@ -0,0 +1,105 @@
+﻿namespace JCNET.字符串处理;
+
+/// <summary>
+///		提供一些扩展的 Trim 方法
+/// </summary>
+public static class TrimExtension
+{
+	/// <summary>
+	///		将 match 之前的头部截掉。
+	///		<br/>* match 是从头部往后查找，找到第一个就算找到。
+	///		<br/>* match 如果为空字符串，视为没有任何匹配，也就是原封不动将 self 返回。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match">从这里之前的部分要被截掉。</param>
+	/// <param name="trim_match">截掉的部分是否包括 match 本身。true 表示包括，false 表示不包括。</param>
+	/// <returns>返回去掉头部之后的结果。如果 match 没有匹配，则没有头部，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimHeadBeforeFirstMatch(this ReadOnlyMemory<char> self,
+		ReadOnlySpan<char> match, bool trim_match)
+	{
+		if (self.Length == 0 || match.Length == 0)
+		{
+			return self;
+		}
+
+		int index = self.IndexOf(match);
+		if (index == -1)
+		{
+			return self;
+		}
+
+		if (trim_match)
+		{
+			// 截掉的部分包括 match 本身
+			return self[(index + match.Length)..];
+		}
+		else
+		{
+			// 截掉的部分不包括 match 本身
+			return self[index..];
+		}
+	}
+
+	/// <summary>
+	///		将 match 之前的头部截掉。
+	///		<br/>* match 是从头部往后查找，找到第一个就算找到。
+	///		<br/>* match 如果为空字符串，视为没有任何匹配，也就是原封不动将 self 返回。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match"></param>
+	/// <param name="trim_match"></param>
+	/// <returns>返回去掉头部之后的结果。如果 match 没有匹配，则没有头部，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimHeadBeforeFirstMatch(this string self,
+		ReadOnlySpan<char> match, bool trim_match)
+	{
+		return self.AsMemory().TrimHeadBeforeFirstMatch(match, trim_match);
+	}
+
+	/// <summary>
+	///		将 self 从 match 处截掉尾巴。
+	///		<br/>* match 是从头部往后查找，找到第一个就算找到。
+	///		<br/>* match 如果为空字符串，视为没有任何匹配，也就是原封不动将 self 返回。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match">从这里往后的尾巴将会被截掉。</param>
+	/// <param name="trim_match">截掉的部分是否包括 match 本身。true 表示包括，false 表示不包括。</param>
+	/// <returns>返回去掉尾巴后的结果。如果 match 没有匹配，则没有尾巴，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimTailAfterFirstMatch(this ReadOnlyMemory<char> self,
+		ReadOnlySpan<char> match, bool trim_match)
+	{
+		if (self.Length == 0 || match.Length == 0)
+		{
+			return self;
+		}
+
+		int index = self.Span.IndexOf(match);
+		if (index == -1)
+		{
+			return self;
+		}
+
+		if (trim_match)
+		{
+			return self[..index];
+		}
+		else
+		{
+			return self[..(index + match.Length)];
+		}
+	}
+
+	/// <summary>
+	///		将 self 从 match 处截掉尾巴。
+	///		<br/>* match 是从头部往后查找，找到第一个就算找到。
+	///		<br/>* match 如果为空字符串，视为没有任何匹配，也就是原封不动将 self 返回。
+	/// </summary>
+	/// <param name="self"></param>
+	/// <param name="match"></param>
+	/// <param name="trim_match"></param>
+	/// <returns>返回去掉尾巴后的结果。如果 match 没有匹配，则没有尾巴，直接返回 self。</returns>
+	public static ReadOnlyMemory<char> TrimTailAfterFirstMatch(this string self,
+		ReadOnlySpan<char> match, bool trim_match)
+	{
+		return self.AsMemory().TrimTailAfterFirstMatch(match, trim_match);
+	}
+}
