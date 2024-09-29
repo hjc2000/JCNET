67d7fb242625fdce6a88aa232725eb4507703a16
hjc2000 <hjc.666@foxmail.com>
2024-09-29 15:09:30 +0800

    ReadOnlyMemory<char> result = str.GetBetween("666", "666");

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,5 @@
 ﻿using JCNET.字符串处理;
 
 string str = "666-中间-666";
-ReadOnlyMemory<char> result = str.GetBetween("666", "777");
+ReadOnlyMemory<char> result = str.GetBetween("666", "666");
 Console.WriteLine(result);
