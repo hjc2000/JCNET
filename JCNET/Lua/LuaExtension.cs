20febd71e961838f4d526b82d7e160dcfba08c10
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-30 23:39:42 +0800

    GetCustomGlobalVariablePaths

--- JCNET/Lua/LuaExtension.cs
+++ JCNET/Lua/LuaExtension.cs
@@ -11,7 +11,7 @@ public static class LuaExtension
 	///		获取 package.path 中的路径。这里面存放的是 require 函数搜索模块的路径。
 	///		自定义的模块路径需要放在这里。
 	/// </summary>
-	/// <param name="self"></param>
+	/// <param path="self"></param>
 	/// <returns></returns>
 	public static string[] GetCustomRequireSearchPath(this NLua.Lua self)
 	{
@@ -21,50 +21,10 @@ public static class LuaExtension
 	}
 
 	/// <summary>
-	///		获取全局变量名列表。
-	/// </summary>
-	/// <param name="self"></param>
-	/// <returns></returns>
-	public static List<string> GetGlobalVariableNames(this NLua.Lua self)
-	{
-		LuaTable table = self.GetTable("_G");
-		Dictionary<object, object> dic = self.GetTableDict(table);
-		List<string> key_lists = [];
-		foreach (string key in dic.Keys)
-		{
-			key_lists.Add(key.ToString());
-		}
-
-		return key_lists;
-	}
-
-	/// <summary>
-	///		获取自定义的全局变量的变量名列表。
-	/// </summary>
-	/// <param name="self"></param>
-	/// <returns></returns>
-	public static List<string> GetCustomGlobalVariableNames(this NLua.Lua self)
-	{
-		/* 所谓自定义，就是比 lua 解释器自带的多出来的部分，所以新建一个干净的 Lua 解释器对象，
-		 * 获取其中的全局变量名，然后获取 self 中的全局变量名，将 self 的全局变量名剔除掉干净
-		 * 的 Lua 解释器对象中的全局变量名，就得到了自定义的全局变量名。
-		 */
-		NLua.Lua lua = new();
-		List<string> origin_global_variable_names = lua.GetGlobalVariableNames();
-		List<string> current_global_variable_names = self.GetGlobalVariableNames();
-		foreach (string name in origin_global_variable_names)
-		{
-			current_global_variable_names.Remove(name);
-		}
-
-		return current_global_variable_names;
-	}
-
-	/// <summary>
 	///		获取一个 lua 表中的内容。
 	/// </summary>
-	/// <param name="self"></param>
-	/// <param name="table"></param>
+	/// <param path="self"></param>
+	/// <param path="table"></param>
 	/// <returns>字典。其中，键为访问该 lua 表中的值的路径，值就是对 lua 表中的值的引用。</returns>
 	public static Dictionary<string, object> GetTableContents(this NLua.Lua self, NLua.LuaTable table)
 	{
@@ -92,5 +52,42 @@ public static class LuaExtension
 		}
 
 		return contents;
+	}
+
+	/// <summary>
+	///		获取 _G 表中的全局变量的路径。
+	/// </summary>
+	/// <param path="self"></param>
+	/// <returns></returns>
+	public static List<string> GetGlobalVariablePaths(this NLua.Lua self)
+	{
+		LuaTable table = self.GetTable("_G");
+		Dictionary<string, object> dic = self.GetTableContents(table);
+		List<string> paths = [.. dic.Keys];
+		return paths;
+	}
+
+	/// <summary>
+	///		获取 _G 表中的用户自定义全局变量的路径。
+	///		<br/>* 所谓自定义，就是比 lua 解释器自带的多出来的部分
+	/// </summary>
+	/// <param path="self"></param>
+	/// <returns></returns>
+	public static List<string> GetCustomGlobalVariablePaths(this NLua.Lua self)
+	{
+		// 全新的空白的 lua 解释器对象。里面只有默认的，自带的全局变量
+		NLua.Lua lua = new();
+		
+		// 提取默认的，自带的全局变量
+		List<string> origin_global_variable_paths = lua.GetGlobalVariablePaths();
+
+		// 提取 self 中的全局变量，然后排除掉 origin_global_variable_paths
+		List<string> current_global_variable_paths = self.GetGlobalVariablePaths();
+		foreach (string path in origin_global_variable_paths)
+		{
+			current_global_variable_paths.Remove(path);
+		}
+
+		return current_global_variable_paths;
 	}
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -3,15 +3,9 @@ using NLua;
 
 Lua lua = new();
 lua.DoFile(@"F:\repos\lua_test\main.lua");
-List<string> list = lua.GetCustomGlobalVariableNames();
+List<string> list = lua.GetGlobalVariablePaths();
 foreach (string s in list)
 {
 	Console.WriteLine(s);
 }
 
-LuaTable table = lua.GetTable("G");
-Dictionary<string, object> table_contents = lua.GetTableContents(table);
-foreach (string s in table_contents.Keys)
-{
-	Console.WriteLine(s);
-}
